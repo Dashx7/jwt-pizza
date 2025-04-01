fc5333eefaa98516d32be136295e0b5e1fc5ecc2
Josh Wiseman <64111981+Dashx7@users.noreply.github.com>
2025-04-01 16:46:50 -0600

    Create loginAndOrder.js

---
+++ loadTests/loginAndOrder.js
@@ -0,0 +1,259 @@
+import { sleep, check, group, fail } from 'k6'
+import http from 'k6/http'
+import jsonpath from 'https://jslib.k6.io/jsonpath/1.0.2/index.js'
+
+export const options = {
+  cloud: {
+    distribution: { 'amazon:us:ashburn': { loadZone: 'amazon:us:ashburn', percent: 100 } },
+    apm: [],
+  },
+  thresholds: {},
+  scenarios: {
+    Scenario_1: {
+      executor: 'ramping-vus',
+      gracefulStop: '30s',
+      stages: [
+        { target: 5, duration: '30s' },
+        { target: 15, duration: '1m' },
+        { target: 10, duration: '30s' },
+        { target: 0, duration: '30s' },
+      ],
+      gracefulRampDown: '30s',
+      exec: 'scenario_1',
+    },
+  },
+}
+
+export function scenario_1() {
+  let response
+
+  const vars = {}
+
+  response = http.put(
+    'https://pizza-service.joshwiseman.click/api/auth',
+    '{"email":"a@jwt.com","password":"admin"}',
+    {
+      headers: {
+        accept: '*/*',
+        'accept-encoding': 'gzip, deflate, br, zstd',
+        'accept-language': 'en-US,en;q=0.9',
+        'content-type': 'application/json',
+        origin: 'https://pizza.joshwiseman.click',
+        priority: 'u=1, i',
+        'sec-ch-ua': '"Chromium";v="134", "Not:A-Brand";v="24", "Google Chrome";v="134"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"macOS"',
+        'sec-fetch-dest': 'empty',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-site': 'same-site',
+      },
+    }
+  )
+    if (!check(response, { 'login check status equals 200': response => response.status.toString() === '200' })) {
+    console.log(response.body);
+    fail('Login was *not* 200');
+  }
+
+  vars['token'] = jsonpath.query(response.json(), '$.token')[0]
+
+  response = http.options('https://pizza-service.joshwiseman.click/api/auth', null, {
+    headers: {
+      accept: '*/*',
+      'accept-encoding': 'gzip, deflate, br, zstd',
+      'accept-language': 'en-US,en;q=0.9',
+      'access-control-request-headers': 'content-type',
+      'access-control-request-method': 'PUT',
+      origin: 'https://pizza.joshwiseman.click',
+      priority: 'u=1, i',
+      'sec-fetch-dest': 'empty',
+      'sec-fetch-mode': 'cors',
+      'sec-fetch-site': 'same-site',
+    },
+  })
+  sleep(3)
+
+  response = http.get('https://pizza-service.joshwiseman.click/api/order/menu', {
+    headers: {
+      accept: '*/*',
+      'accept-encoding': 'gzip, deflate, br, zstd',
+      'accept-language': 'en-US,en;q=0.9',
+      authorization: `Bearer ${vars['token']}`,
+      'content-type': 'application/json',
+      'if-none-match': 'W/"1fc-cgG/aqJmHhElGCplQPSmgl2Gwk0"',
+      origin: 'https://pizza.joshwiseman.click',
+      priority: 'u=1, i',
+      'sec-ch-ua': '"Chromium";v="134", "Not:A-Brand";v="24", "Google Chrome";v="134"',
+      'sec-ch-ua-mobile': '?0',
+      'sec-ch-ua-platform': '"macOS"',
+      'sec-fetch-dest': 'empty',
+      'sec-fetch-mode': 'cors',
+      'sec-fetch-site': 'same-site',
+    },
+  })
+
+  response = http.options('https://pizza-service.joshwiseman.click/api/order/menu', null, {
+    headers: {
+      accept: '*/*',
+      'accept-encoding': 'gzip, deflate, br, zstd',
+      'accept-language': 'en-US,en;q=0.9',
+      'access-control-request-headers': 'authorization,content-type',
+      'access-control-request-method': 'GET',
+      origin: 'https://pizza.joshwiseman.click',
+      priority: 'u=1, i',
+      'sec-fetch-dest': 'empty',
+      'sec-fetch-mode': 'cors',
+      'sec-fetch-site': 'same-site',
+    },
+  })
+
+  response = http.get('https://pizza-service.joshwiseman.click/api/franchise', {
+    headers: {
+      accept: '*/*',
+      'accept-encoding': 'gzip, deflate, br, zstd',
+      'accept-language': 'en-US,en;q=0.9',
+      authorization: `Bearer ${vars['token']}`,
+      'content-type': 'application/json',
+      'if-none-match': 'W/"40-EPPawbPn0KtYVCL5qBynMCqA1xo"',
+      origin: 'https://pizza.joshwiseman.click',
+      priority: 'u=1, i',
+      'sec-ch-ua': '"Chromium";v="134", "Not:A-Brand";v="24", "Google Chrome";v="134"',
+      'sec-ch-ua-mobile': '?0',
+      'sec-ch-ua-platform': '"macOS"',
+      'sec-fetch-dest': 'empty',
+      'sec-fetch-mode': 'cors',
+      'sec-fetch-site': 'same-site',
+    },
+  })
+
+  response = http.options('https://pizza-service.joshwiseman.click/api/franchise', null, {
+    headers: {
+      accept: '*/*',
+      'accept-encoding': 'gzip, deflate, br, zstd',
+      'accept-language': 'en-US,en;q=0.9',
+      'access-control-request-headers': 'authorization,content-type',
+      'access-control-request-method': 'GET',
+      origin: 'https://pizza.joshwiseman.click',
+      priority: 'u=1, i',
+      'sec-fetch-dest': 'empty',
+      'sec-fetch-mode': 'cors',
+      'sec-fetch-site': 'same-site',
+    },
+  })
+  sleep(7.8)
+
+  response = http.post(
+    'https://pizza-service.joshwiseman.click/api/order',
+    '{"items":[{"menuId":1,"description":"Veggie","price":0.0038},{"menuId":1,"description":"Veggie","price":0.0038},{"menuId":1,"description":"Veggie","price":0.0038},{"menuId":2,"description":"Pepperoni","price":0.0042}],"storeId":"1","franchiseId":1}',
+    {
+      headers: {
+        accept: '*/*',
+        'accept-encoding': 'gzip, deflate, br, zstd',
+        'accept-language': 'en-US,en;q=0.9',
+        authorization: `Bearer ${vars['token']}`,
+        'content-type': 'application/json',
+        origin: 'https://pizza.joshwiseman.click',
+        priority: 'u=1, i',
+        'sec-ch-ua': '"Chromium";v="134", "Not:A-Brand";v="24", "Google Chrome";v="134"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"macOS"',
+        'sec-fetch-dest': 'empty',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-site': 'same-site',
+      },
+    }
+  )
+
+  vars['jwt1'] = jsonpath.query(response.json(), '$.jwt')[0]
+    if (!check(response, { 'Pizza order status equals 200': response => response.status.toString() === '200' })) {
+    console.log(response.body);
+    fail('Login was *not* 200');
+  }
+
+  response = http.options('https://pizza-service.joshwiseman.click/api/order', null, {
+    headers: {
+      accept: '*/*',
+      'accept-encoding': 'gzip, deflate, br, zstd',
+      'accept-language': 'en-US,en;q=0.9',
+      'access-control-request-headers': 'authorization,content-type',
+      'access-control-request-method': 'POST',
+      origin: 'https://pizza.joshwiseman.click',
+      priority: 'u=1, i',
+      'sec-fetch-dest': 'empty',
+      'sec-fetch-mode': 'cors',
+      'sec-fetch-site': 'same-site',
+    },
+  })
+  sleep(3.5)
+
+  response = http.post(
+    'https://pizza-factory.cs329.click/api/order/verify',
+    `{"jwt":"${vars['jwt1']}"}`,
+    {
+      headers: {
+        accept: '*/*',
+        'accept-encoding': 'gzip, deflate, br, zstd',
+        'accept-language': 'en-US,en;q=0.9',
+        authorization: `Bearer ${vars['token']}`,
+        'content-type': 'application/json',
+        origin: 'https://pizza.joshwiseman.click',
+        priority: 'u=1, i',
+        'sec-ch-ua': '"Chromium";v="134", "Not:A-Brand";v="24", "Google Chrome";v="134"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"macOS"',
+        'sec-fetch-dest': 'empty',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-site': 'cross-site',
+        'sec-fetch-storage-access': 'active',
+      },
+    }
+  )
+
+  response = http.options('https://pizza-factory.cs329.click/api/order/verify', null, {
+    headers: {
+      accept: '*/*',
+      'accept-encoding': 'gzip, deflate, br, zstd',
+      'accept-language': 'en-US,en;q=0.9',
+      'access-control-request-headers': 'authorization,content-type',
+      'access-control-request-method': 'POST',
+      origin: 'https://pizza.joshwiseman.click',
+      priority: 'u=1, i',
+      'sec-fetch-dest': 'empty',
+      'sec-fetch-mode': 'cors',
+      'sec-fetch-site': 'cross-site',
+    },
+  })
+  sleep(12.5)
+
+  response = http.del('https://pizza-service.joshwiseman.click/api/auth', null, {
+    headers: {
+      accept: '*/*',
+      'accept-encoding': 'gzip, deflate, br, zstd',
+      'accept-language': 'en-US,en;q=0.9',
+      authorization: `Bearer ${vars['token']}`,
+      'content-type': 'application/json',
+      origin: 'https://pizza.joshwiseman.click',
+      priority: 'u=1, i',
+      'sec-ch-ua': '"Chromium";v="134", "Not:A-Brand";v="24", "Google Chrome";v="134"',
+      'sec-ch-ua-mobile': '?0',
+      'sec-ch-ua-platform': '"macOS"',
+      'sec-fetch-dest': 'empty',
+      'sec-fetch-mode': 'cors',
+      'sec-fetch-site': 'same-site',
+    },
+  })
+
+  response = http.options('https://pizza-service.joshwiseman.click/api/auth', null, {
+    headers: {
+      accept: '*/*',
+      'accept-encoding': 'gzip, deflate, br, zstd',
+      'accept-language': 'en-US,en;q=0.9',
+      'access-control-request-headers': 'authorization,content-type',
+      'access-control-request-method': 'DELETE',
+      origin: 'https://pizza.joshwiseman.click',
+      priority: 'u=1, i',
+      'sec-fetch-dest': 'empty',
+      'sec-fetch-mode': 'cors',
+      'sec-fetch-site': 'same-site',
+    },
+  })
+}
